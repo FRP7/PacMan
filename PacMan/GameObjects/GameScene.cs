5b8fa7e62499638bfe6fb82d6d6c3529e5742fe1
Francisco <frelvaspires@gmail.com>
2021-07-02 17:04:13 +0100

    Initialize tiles in GameScene finished.

--- PacMan/GameObjects/GameScene.cs
+++ PacMan/GameObjects/GameScene.cs
@@ -20,202 +20,173 @@ namespace PacMan.GameObjects
 
         public override void Render()
         {
-            Scene();
+            UI();
+
+            foreach (Tile item in tiles)
+            {
+                Console.SetCursorPosition(item.X, item.Y);
+                Console.Write(item.Sprite);
+            }
         }
 
         private void InitializeTiles()
         {
             tiles = new List<Tile>();
-        }
-
-        private void Scene()
-        {
-            // topo
-            Console.SetCursorPosition(0, 0);
-            Console.Write("High Score: ");
 
             // lado esquerdo
             for (int i = 2; i < 25; i++)
             {
-                Console.SetCursorPosition(0, i);
-                Console.Write('|');
+                tiles.Add(new Tile(0, i, '|'));
             }
 
             // lado direito
             for (int i = 2; i < 25; i++)
             {
-                Console.SetCursorPosition(70, i);
-                Console.Write('|');
+                tiles.Add(new Tile(70, i, '|'));
             }
 
             // lado de cima
             for (int i = 0; i < 71; i++)
             {
-                Console.SetCursorPosition(i, 1);
-                Console.Write('_');
+                tiles.Add(new Tile(i, 1, '_'));
             }
 
             // lado de baixo
             for (int i = 0; i < 71; i++)
             {
-                Console.SetCursorPosition(i, 24);
-                Console.Write('_');
+                tiles.Add(new Tile(i, 24, '_'));
             }
 
             // obstaculo 1
             for (int i = 7; i < 27; i++)
             {
-                Console.SetCursorPosition(i, 4);
-                Console.Write('_');
+                tiles.Add(new Tile(i, 4, '_'));
             }
+
             for (int i = 7; i < 27; i++)
             {
-                Console.SetCursorPosition(i, 5);
-                Console.Write('_');
+                tiles.Add(new Tile(i, 5, '_'));
             }
-            Console.SetCursorPosition(6, 5);
-            Console.Write('|');
-            Console.SetCursorPosition(27, 5);
-            Console.Write('|');
+
+            tiles.Add(new Tile(6, 5, '|'));
+            tiles.Add(new Tile(27, 5, '|'));
 
             // obstaculo 2
             for (int i = 2; i < 6; i++)
             {
-                Console.SetCursorPosition(34, i);
-                Console.Write('|');
+                tiles.Add(new Tile(34, i, '|'));
             }
             for (int i = 2; i < 6; i++)
             {
-                Console.SetCursorPosition(36, i);
-                Console.Write('|');
+                tiles.Add(new Tile(36, i, '|'));
             }
-            Console.SetCursorPosition(35, 5);
-            Console.Write('_');
+            tiles.Add(new Tile(35, 5, '_'));
 
             // obstaculo 3
             for (int i = 44; i < 63; i++)
             {
-                Console.SetCursorPosition(i, 4);
-                Console.Write('_');
+                tiles.Add(new Tile(i, 4, '_'));
             }
             for (int i = 44; i < 63; i++)
             {
-                Console.SetCursorPosition(i, 5);
-                Console.Write('_');
+                tiles.Add(new Tile(i, 5, '_'));
             }
-            Console.SetCursorPosition(43, 5);
-            Console.Write('|');
-            Console.SetCursorPosition(63, 5);
-            Console.Write('|');
+            tiles.Add(new Tile(43, 5, '|'));
+            tiles.Add(new Tile(63, 5, '|'));
 
             //obstaculo 4
             for (int i = 9; i < 21; i++)
             {
-                Console.SetCursorPosition(6, i);
-                Console.Write('|');
+                tiles.Add(new Tile(6, i, '|'));
             }
             for (int i = 9; i < 21; i++)
             {
-                Console.SetCursorPosition(8, i);
-                Console.Write('|');
+                tiles.Add(new Tile(8, i, '|'));
             }
-            Console.SetCursorPosition(7, 8);
-            Console.Write('_');
-            Console.SetCursorPosition(7, 20);
-            Console.Write('_');
+            tiles.Add(new Tile(7, 8, '_'));
+            tiles.Add(new Tile(7, 20, '_'));
 
             // obstaculo 5
             for (int i = 9; i < 15; i++)
             {
-                Console.SetCursorPosition(15, i);
-                Console.Write('|');
+                tiles.Add(new Tile(15, i, '|'));
             }
             for (int i = 10; i < 14; i++)
             {
-                Console.SetCursorPosition(17, i);
-                Console.Write('|');
+                tiles.Add(new Tile(17, i, '|'));
             }
             for (int i = 10; i < 14; i++)
             {
-                Console.SetCursorPosition(25, i);
-                Console.Write('|');
+                tiles.Add(new Tile(25, i, '|'));
             }
             for (int i = 9; i < 21; i++)
             {
-                Console.SetCursorPosition(27, i);
-                Console.Write('|');
+                tiles.Add(new Tile(27, i, '|'));
             }
             for (int i = 15; i < 28; i++)
             {
-                Console.SetCursorPosition(i, 8);
-                Console.Write('_');
+                tiles.Add(new Tile(i, 8, '_'));
             }
             for (int i = 18; i < 25; i++)
             {
-                Console.SetCursorPosition(i, 9);
-                Console.Write('_');
+                tiles.Add(new Tile(i, 9, '_'));
             }
             for (int i = 18; i < 25; i++)
             {
-                Console.SetCursorPosition(i, 13);
-                Console.Write('_');
+                tiles.Add(new Tile(i, 13, '_'));
             }
             for (int i = 15; i < 25; i++)
             {
-                Console.SetCursorPosition(i, 14);
-                Console.Write('_');
+                tiles.Add(new Tile(i, 14, '_'));
             }
             for (int i = 15; i < 21; i++)
             {
-                Console.SetCursorPosition(25, i);
-                Console.Write('|');
+                tiles.Add(new Tile(25, i, '|'));
             }
-            Console.SetCursorPosition(26, 20);
-            Console.Write('_');
+            tiles.Add(new Tile(26, 20, '_'));
 
             // obstaculo 6
             for (int i = 9; i < 21; i++)
             {
-                Console.SetCursorPosition(34, i);
-                Console.Write('|');
+                tiles.Add(new Tile(34, i, '|'));
             }
             for (int i = 9; i < 21; i++)
             {
-                Console.SetCursorPosition(44, i);
-                Console.Write('|');
+                tiles.Add(new Tile(44, i, '|'));
             }
             for (int i = 34; i < 45; i++)
             {
-                Console.SetCursorPosition(i, 8);
-                Console.Write('_');
+                tiles.Add(new Tile(i, 8, '_'));
             }
             for (int i = 34; i < 45; i++)
             {
-                Console.SetCursorPosition(i, 20);
-                Console.Write('_');
+                tiles.Add(new Tile(i, 20, '_'));
             }
 
             // obstaculo 7
             for (int i = 9; i < 21; i++)
             {
-                Console.SetCursorPosition(53, i);
-                Console.Write('|');
+                tiles.Add(new Tile(53, i, '|'));
             }
             for (int i = 9; i < 21; i++)
             {
-                Console.SetCursorPosition(63, i);
-                Console.Write('|');
+                tiles.Add(new Tile(63, i, '|'));
             }
             for (int i = 53; i < 64; i++)
             {
-                Console.SetCursorPosition(i, 8);
-                Console.Write('_');
+                tiles.Add(new Tile(i, 8, '_'));
             }
             for (int i = 53; i < 64; i++)
             {
-                Console.SetCursorPosition(i, 20);
-                Console.Write('_');
+                tiles.Add(new Tile(i, 20, '_'));
             }
+        }
+
+        private void UI()
+        {
+            // topo
+            Console.SetCursorPosition(0, 0);
+            Console.Write("High Score: ");
         }
     }
 }
